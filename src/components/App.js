bfcbc332d61a8f65255d1cb90dc301fcce29094a
Nick Dean <klick5000@gmail.com>
2019-03-28 01:10:36 -0600

    created list of bills

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import BillList from './BillList';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <BillList />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default App; 
+export default App;
---
+++ src/components/BillList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBill } from '../actions';
+
+class BillList extends Component {
+  state = {};
+  renderList() {
+    return this.props.bills.map((bill) => {
+      return (
+        <div className="item" key={bill.payee}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectBill(bill)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{bill.payee}</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return { bills: state.bills };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    selectBill,
+  },
+)(BillList);
